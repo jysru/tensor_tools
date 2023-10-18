a399112a0e133b0438059f09f26ebe921b04ae54
JSaucourt <jysru@pm.me>
2023-10-18 17:32:30 +0200

    fixed torch metrics

--- tensortools/torch/metrics.py
+++ tensortools/torch/metrics.py
@@ -1,9 +1,7 @@
-import numpy as np
 import torch
-import tensorflow as tf
 
 
-def pearson(x, y, squared: bool = False, inversed: bool = False) -> torch.float:
+def pearson(x: torch.Tensor, y: torch.Tensor, squared: bool = False, inversed: bool = False) -> torch.float:
     x, y = torch.abs(x), torch.abs(y)
     if squared:
             x, y = torch.square(x), torch.square(y)
@@ -12,38 +10,28 @@ def pearson(x, y, squared: bool = False, inversed: bool = False) -> torch.float:
     return 1 - p if inversed else p
 
 
-def quality(x, y, inversed: bool = True, squared: bool = True):
-        prod = torch.sum(x * torch.conj(y))
-        norm = torch.sum(torch.abs(x) * torch.abs(y))
-        q = torch.abs(prod / norm)
-
-        if squared:
-            q = torch.square(q)
-        if inversed:
-            q = 1 - q
-        return q
-
-
-def mae_numpy(x: np.ndarray, y: np.ndarray):
+def mae(x: torch.Tensor, y: torch.Tensor) -> torch.float:
     """Return the mean absolute error between the two arrays."""
-    if np.iscomplexobj(x):
-        x = np.abs(x)
-    if np.iscomplexobj(y):
-        y = np.abs(y)
-    return np.mean(np.abs(x - y))
+    x, y = torch.abs(x), torch.abs(y)
+    return torch.mean(torch.abs(x - y))
 
 
-def mse_numpy(x: np.ndarray, y: np.ndarray):
+def mse(x: torch.Tensor, y: torch.Tensor) -> torch.float:
     """Return the mean square error between the two arrays."""
-    if np.iscomplexobj(x):
-        x = np.abs(x)
-    if np.iscomplexobj(y):
-        y = np.abs(y)
-    return np.mean(np.square(x - y))
+    x, y = torch.abs(x), torch.abs(y)
+    return torch.mean(torch.square(x - y))
 
 
-def dot_product_numpy(x: np.ndarray, y: np.ndarray, normalized: bool = True):
+def dot_product(x: torch.Tensor, y: torch.Tensor, normalized: bool = True) -> torch.float:
     """Return the scalar product between the two complex arrays."""
-    prod = np.sum(x * np.conjugate(y))
-    norm = np.sum(np.abs(x) * np.abs(y))
+    prod = torch.sum(x * torch.conj(y))
+    norm = torch.sum(torch.abs(x) * torch.abs(y))
     return prod / norm if normalized else prod
+
+
+def quality(x: torch.Tensor, y: torch.Tensor, squared: bool = True, inversed: bool = True) -> torch.float:
+    """Return the magnitude of the normalized dot product between the two complex arrays."""
+    q = torch.abs(dot_product(x, y, normalized=True))
+    if squared:
+        q = torch.square(q)
+    return 1 - q if inversed else q
